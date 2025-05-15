398492daa42241036499b0ca74fe31f4bb2a4440
Alexis RARCHAERT <bonjour@alexis-rarchaert.fr>
2025-05-15 17:43:35 +0200

    Remove .env from repository

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({ message: 'API en ligne ! 🚀' });
-});
-
-module.exports = router;
--- src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`\x1b[32mServer is running on port ${PORT}\x1b[0m`);
-});
