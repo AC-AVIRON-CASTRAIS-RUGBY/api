593b1cfc46758b664af7cc932dffec6150df05a4
Alexis RARCHAERT <bonjour@alexis-rarchaert.fr>
2025-05-15 17:15:39 +0200

    ➕ Ajout des routes et du contrôleur pour la gestion des phases

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const tournamentRoutes = require('./routes/tournamentRoutes');
 const poolRoutes = require('./routes/poolRoutes');
 const teamRoutes = require('./routes/teamRoutes');
 const gameRoutes = require('./routes/gameRoutes');
+const phaseRoutes = require('./routes/phaseRoutes');
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.use('/api/tournaments', tournamentRoutes);
 app.use('/api/pools', poolRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/games', gameRoutes);
+app.use('/api/phases', phaseRoutes);
 
 module.exports = app;
