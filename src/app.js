089550dc0868311172ea8f0e189e127998ab989a
Alexis RARCHAERT <bonjour@alexis-rarchaert.fr>
2025-05-15 17:11:37 +0200

    ➕ Ajout des routes et du contrôleur pour la gestion des équipes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const routes = require('./routes/index');
 
 const tournamentRoutes = require('./routes/tournamentRoutes');
 const poolRoutes = require('./routes/poolRoutes');
+const teamRoutes = require('./routes/teamRoutes');
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use('/', routes);
 app.use('/api/tournaments', tournamentRoutes);
 app.use('/api/pools', poolRoutes);
+app.use('/api/teams', teamRoutes);
 
 module.exports = app;
