4217e2ee7c60355ee33cd8e983ec5ec7e2be60fe
Alexis Rarchaert <bonjour@alexis-rarchaert.fr>
2025-06-07 11:05:18 +0200

    Update poolRoutes.js

--- src/routes/poolRoutes.js
+++ src/routes/poolRoutes.js
@@ -233,6 +233,6 @@ router.get('/tournaments/:tournamentId/:id/standings', poolController.getPoolSta
  *       200:
  *         description: Classement général récupéré avec succès
  */
-router.get('/tournaments/standings/:tournamentId/', poolController.getTournamentStandings);
+router.get('/standings/:tournamentId', poolController.getTournamentStandings);
 
 module.exports = router;
